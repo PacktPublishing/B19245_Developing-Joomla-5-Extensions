a27dd53a0f577cab7017dc9ba3834209a219f112
Carlos Cámara <1695138+carcam@users.noreply.github.com>
2024-09-15 15:59:28 +0200

    Fixing modal layout display

--- src/component/admin/src/View/Projects/HtmlView.php
+++ src/component/admin/src/View/Projects/HtmlView.php
@@ -4,11 +4,17 @@ namespace Piedpiper\Component\Spm\Administrator\View\Projects;
 
 \defined('_JEXEC') or die;
 
+use Joomla\CMS\Factory;
 use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
 use Joomla\CMS\MVC\View\GenericDataException;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Toolbar\Toolbar;
+use Joomla\CMS\Toolbar\ToolbarHelper;
+
 
 class HtmlView extends BaseHtmlView
 {
+    public $filterForm;
     public $state;
     public $items=[];
     public $pagination;
@@ -24,10 +30,33 @@ class HtmlView extends BaseHtmlView
         $this->filterForm    = $this->get('FilterForm');
         $this->activeFilters = $this->get('ActiveFilters');
 
-        if (count($errors = $this->get('Errors'))) {
+        if (count($errors = $this->get('Errors')))
+        {
             throw new GenericDataException(implode("\n", $errors), 500);
         }
 
+        if ($this->getLayout() !== 'modal') {
+            $this->addToolbar();
+        }
+
         parent::display($tpl);
+    }
+
+    /**
+     * Add the page title and toolbar.
+     *
+     * @return void
+     *
+     */
+    protected function addToolbar(): void
+    {
+        $user  = Factory::getApplication()->getIdentity();
+        ToolbarHelper::title(Text::_('COM_SPM_PROJECTS'), 'bookmark banners-levels');
+
+        $toolbar = Toolbar::getInstance('toolbar');
+
+        if ($user->authorise('core.admin', 'com_spm') || $user->authorise('core.options', 'com_spm')) {
+            $toolbar->preferences('com_spm');
+        }
     }
 }
